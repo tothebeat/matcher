bd86fe591e23817633d2e07d28c6d58c8e695dd6
nick <nick@goggl.es>
2013-09-30 19:08:08 -0500

    Added Bootstrap and DataTables.

--- matcher/settings.py
+++ matcher/settings.py
@@ -1,4 +1,5 @@
 # Django settings for matcher project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -8,6 +9,8 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 DATABASES = {
     'default': {
@@ -61,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'htdocs')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -72,6 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -112,6 +116,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
--- spreadsheets/models.py
+++ spreadsheets/models.py
@@ -18,12 +18,12 @@ class DataSet(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse_lazy('dataset-detail', args=[self.pk]) 
+        return reverse_lazy('dataset-detail', args=[self.pk])
 
 
 class Spreadsheet(models.Model):
     '''
-    At core, an XLSX file. Wrapped with methods to act like 
+    At core, an XLSX file. Wrapped with methods to act like
     a terrible, terrible database. No CRUD here. 
     '''
     _file = models.FileField(upload_to='documents/%Y/%m/%d/')
@@ -41,15 +41,13 @@ class Spreadsheet(models.Model):
         workbook = xlrd.open_workbook(file_contents=self._file.read())
         self._file.seek(0)
         worksheet = workbook.sheet_by_index(0)
-        number_of_rows = worksheet.nrows
-        return number_of_rows
+        return worksheet.nrows
 
     def number_of_rows(self):
         workbook = xlrd.open_workbook(file_contents=self._file.read())
         self._file.seek(0)
         worksheet = workbook.sheet_by_index(0)
-        number_of_columns = worksheet.ncols
-        return number_of_columns
+        return worksheet.ncols
 
     def row(self, row_number):
         workbook = xlrd.open_workbook(file_contents=self._file.read())
@@ -65,30 +63,27 @@ class Spreadsheet(models.Model):
         column = map(lambda cell: cell.value, worksheet.col(column_number))
         return column
 
-    def data(self):
-        workbook = xlrd.open_workbook(file_contents=self._file.read())
-        self._file.seek(0)
-        worksheet = workbook.sheet_by_index(0)
-        data = [[worksheet.cell(row_index, col_index).value
-            for col_index in xrange(worksheet.ncols)] 
-            for row_index in xrange(worksheet.nrows)]
-        return data
+    def header_row(self):
+        '''
+        Just the values in the first row that act as column headers,
+        not any of the data.
+        '''
+        return self.row(0)
+
+    def data_rows(self):
+        '''
+        Just the values, not including the header row.
+        '''
+        return [self.row(n) for n in xrange(1, self.number_of_rows())]
 
     def data_as_dicts(self):
         '''
         Assuming the first row in the data is the label/header row, and
         every subsequent row can be considered a data row.
         '''
-        workbook = xlrd.open_workbook(file_contents=self._file.read())
-        self._file.seek(0)
-        worksheet = workbook.sheet_by_index(0)
-        header_row = map(lambda cell: cell.value, worksheet.row(0))
-        data = []
-        for row_index in xrange(1, worksheet.nrows):
-            record = dict(zip(header_row, map(lambda cell: cell.value, worksheet.row(row_index))))
-            data.append(record)
-        return data
+        header_row = self.header_row()
+        data_rows = self.data_rows()
+        return [dict(zip(header_row, data_row)) for data_row in data_rows]
 
     def get_absolute_url(self):
-        return reverse_lazy('dataset-detail', args=[self.pk]) 
-
+        return reverse_lazy('dataset-detail', args=[self.pk])
